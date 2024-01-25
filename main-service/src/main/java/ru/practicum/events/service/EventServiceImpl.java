cf2505ce1e57c03f3af3b0470e90a32abe7f8ea4
Artem <tillerman12@gmail.com>
2024-01-25 17:11:16 +0300

    unique

--- main-service/src/main/java/ru/practicum/events/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/events/service/EventServiceImpl.java
@@ -127,8 +127,8 @@ public class EventServiceImpl implements EventService {
             event.setParticipantLimit(updateEventUserRequest.getParticipantLimit());
         }
 
-        if (updateEventUserRequest.getRequestModeration() != null) {
-            event.setRequestModeration(updateEventUserRequest.getRequestModeration());
+        if (updateEventUserRequest.getRequestModerationRequired() != null) {
+            event.setRequestModeration(updateEventUserRequest.getRequestModerationRequired());
         }
 
         if (updateEventUserRequest.getStateAction() != null) {
@@ -204,8 +204,8 @@ public class EventServiceImpl implements EventService {
             event.setParticipantLimit(updateEventAdminRequest.getParticipantLimit());
         }
 
-        if (updateEventAdminRequest.getRequestModeration() != null) {
-            event.setRequestModeration(updateEventAdminRequest.getRequestModeration());
+        if (updateEventAdminRequest.getRequestModerationRequired() != null) {
+            event.setRequestModeration(updateEventAdminRequest.getRequestModerationRequired());
         }
 
         if (updateEventAdminRequest.getTitle() != null) {
